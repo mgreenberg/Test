51384749626c4679ef369e9cc89dd92766773830
Michael D. Greenberg <mdgreenb@u.northwestern.edu>
2012-03-28 17:18:29 -0500

    init

---
+++ goalTracker/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoalTracker::Application.config.secret_token = '2a20088e02ab2dd7512fc03b16f914235a28ef7c3209f5e1b5bbdf598bceae878e19720141226bb4780f29239ae6bde9db44c4aaf107502b0678dabd2ca2f3ba'
